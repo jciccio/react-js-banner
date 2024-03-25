c0aa5a31eb7444f176180f4d3cf9822c2df52cd8
Jose Antonio Ciccio <jciccio@gmail.com>
2024-03-25 08:19:48 -0600

    React example update to v18

--- react-js-banner-example/src/index.js
+++ react-js-banner-example/src/index.js
@@ -4,13 +4,15 @@ import React from 'react';
 
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client';
 
 import App from "./App";
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <StrictMode>
+const root = createRoot(rootElement);
+root.render( <StrictMode>
     <App />
-  </StrictMode>,
-  rootElement
+  </StrictMode>
 );
+
+
